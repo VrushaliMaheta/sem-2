92543a1f28c85111826075122532292ba625f4e7
VrushaliMaheta <106863736+VrushaliMaheta@users.noreply.github.com>
2022-06-11 10:45:59 +0530

    Update practical1.java

--- practical1.java
+++ practical1.java
@@ -2,7 +2,9 @@ class practical1
 {
   public static void main(String[] args)
   {
-    String str = "Vrushali Maheta";
-    System.out.println("Your Name is : "+str);
+    int a = 10;
+    int b = 20;
+    int c = a+b; 
+    System.out.println("Addition = "+c);
   }
 }
